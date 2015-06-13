5727097f8effad5e6ea5d594b2629ab5ae1e46c0
Jhonatan Laguna <JhonLP@users.noreply.github.com>
2015-06-12 21:46:04 -0700

    Desactivado modo Debug

--- _ProyectoInicial_/settings.py
+++ _ProyectoInicial_/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p^grx$94)ky8ck+zv%&uwhma@k_my4z#1)u9nhqr4tm@l$92$n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
